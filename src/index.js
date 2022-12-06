46f41c39ec5c41f3647d1b74539dea5a9b8f5853
hassan kazungu <hassankazungu01@gmail.com>
2022-12-06 12:36:06 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+const taskForm = document.querySelector('#create-task-form')
+const taskUl = document.querySelector('#tasks')
+
+taskForm.addEventListener('submit', grabTaskFromForm)
+  
+function grabTaskFromForm(e) {
+  e.preventDefault()
+  let inputValue = e.target["new-task-description"].value
+  createAndAppendLi(inputValue)
+  e.target.reset()
+}
+
+function createAndAppendLi(inputValue) {
+  let li = document.createElement('li')
+  li.textContent = inputValue
+  taskUl.append(li)
+
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = " X"
+  li.append(btn)
+} 
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
